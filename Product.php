fbc7e3cd0a325ed654793bce8790fb3e4617f5ae
Joe <152248063+gentle-04muscles@users.noreply.github.com>
2023-12-28 19:05:14 +0600

    Create Product.php

---
+++ Product.php
@@ -0,0 +1,16 @@
+<!-- Product.php -->
+<?php
+class Product {
+    private $name;
+    private $price;
+
+    public function __construct($name, $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function __toString() {
+        return "Name: $this->name, Price: $this->price";
+    }
+}
+?>
